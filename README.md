c62f7c1c156986ebb80dd0fee4884d1979b17f33
JellyDai <daiguo1003@gmail.com>
2024-06-07 21:15:38 +0800

    新增图片比例设置

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # Elementor-Pro-Max
-在Elementor 基础进行拓展(挤牙膏太厉害，只能自己写了)
-详细请查看[changelog.txt](https://github.com/GuodongDai2113/Elementor-Pro-Max/blob/main/changelog.txt)
+在Elementor 基础进行拓展 (挤牙膏太厉害，只能自己写了)
+
+详细功能请查看[changelog.txt](https://github.com/GuodongDai2113/Elementor-Pro-Max/blob/main/changelog.txt)
+
+## 注意事项
+
+本插件可能有很多
+
+```
+开发当前环境如下:
+
+操作系统:Windows 11 专业版 24H2
+
+Apache/2.4.56 (Win64) OpenSSL/1.1.1t PHP/8.2.4
+
+MySQL 版本 : mariadb.org binary distribution v10.4.28
+
+PHP 版本 : 8.2.4
+
+Wordpress 版本 : 6.5.4
+
+Elementor 版本 : 3.21.8
+
+Elementor Pro 版本 : 3.21.0
+```
--- includes/controls/inject.php
+++ includes/controls/inject.php
@@ -70,19 +70,69 @@ class Inject_Controls
         $element->add_control(
             'image_overflow',
             [
-                'label' => esc_html__('Overflow', 'elementor-pro-max'),
-                'description' => esc_html__('By Elementor Pro Max 用于解决动画超出问题', 'elementor-pro-max'),
+                'label' => esc_html__('溢出', 'elementor-pro-max'),
+                'description' => esc_html__('By Elementor Pro Max 用于解决动画超出问题, 设定下方比例也可实现。', 'elementor-pro-max'),
                 'type' => \Elementor\Controls_Manager::SELECT,
                 'default' => '',
                 'options' => [
-                    '' => esc_html__('Default', 'elementor-pro-max'),
-                    'hidden' => esc_html__('hidden', 'elementor-pro-max'),
+                    '' => esc_html__('默认', 'elementor-pro-max'),
+                    'hidden' => esc_html__('隐藏', 'elementor-pro-max'),
                 ],
                 'selectors' => [
                     '{{WRAPPER}}' => 'overflow: {{VALUE}};',
                     '{{WRAPPER}} .elementor-image-box-img' => 'overflow: {{VALUE}};',
                 ]
             ],
+        );
+
+        $element->add_control(
+			'ratio',
+			[
+				'label' => esc_html__( '比例', 'elementor-pro-max' ),
+                'description' => esc_html__('By Elementor Pro Max 设定后最好不要调节图片大小有关参数，可以修改高级设置里的宽度。', 'elementor-pro-max'),
+				'type' => \Elementor\Controls_Manager::SELECT,
+				'default' => '',
+				'options' => [
+					'' => esc_html__( '默认', 'elementor-pro-max' ),
+					'16 / 9' => esc_html__( '16:9', 'elementor-pro-max' ),
+					'9 / 16' => esc_html__( '9:16', 'elementor-pro-max' ),
+					'3 / 4'  => esc_html__( '3:4', 'elementor-pro-max' ),
+					'4 / 3'  => esc_html__( '4:3', 'elementor-pro-max' ),
+					'1 / 1' => esc_html__( '1:1', 'elementor-pro-max' ),
+				],
+				'selectors' => [
+					'{{WRAPPER}}' => 'width:100%;aspect-ratio:{{VALUE}};overflow:hidden;',
+					'{{WRAPPER}} img' => 'width:100%;height:100%;object-fit: cover;',
+				],
+			]
+		);
+
+        $element->add_control(
+            'ratio_object-position',
+            [
+                'label' => esc_html__( '对象位置', 'elementor-pro-max' ),
+                'description' => esc_html__('By Elementor Pro Max', 'elementor-pro-max'),
+                'type' => \Elementor\Controls_Manager::SELECT,
+                'options' => [
+					'center center' => esc_html__( '中心居中', 'elementor-pro-max' ),
+					'center left' => esc_html__( '中间左侧', 'elementor-pro-max' ),
+					'center right' => esc_html__( '中间右侧', 'elementor-pro-max' ),
+					'top center' => esc_html__( '顶部居中', 'elementor-pro-max' ),
+					'top left' => esc_html__( '顶部左侧', 'elementor-pro-max' ),
+					'top right' => esc_html__( '顶部右侧', 'elementor-pro-max' ),
+					'bottom center' => esc_html__( '底部居中', 'elementor-pro-max' ),
+					'bottom left' => esc_html__( '底部左侧', 'elementor-pro-max' ),
+					'bottom right' => esc_html__( '底部右侧', 'elementor-pro-max' ),
+				],
+                'default' => 'center center',
+                'selectors' => [
+					'{{WRAPPER}} img' => 'object-position: {{VALUE}};',
+				],
+                'condition'=>[
+                    'ratio!' => ''
+                ],
+
+            ]
         );
     }
 
